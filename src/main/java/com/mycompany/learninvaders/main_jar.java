c52984f9d32ca1ba16e504c6b5e62971ac89ccd7
S4muelDP <136473236+S4muelDP@users.noreply.github.com>
2023-06-27 22:19:31 -0500

    nuevo main para ,jar
    Se crea un nuevo main para poder crear el artifact .jar

---
+++ src/main/java/com/mycompany/learninvaders/main_jar.java
@@ -0,0 +1,14 @@
+package com.mycompany.learninvaders;
+
+/**
+ *
+ * @author S4muelDP
+ */
+
+public class main_jar {
+    public static void main(String[] args){
+        App.main(args);
+    }
+
+}
+
